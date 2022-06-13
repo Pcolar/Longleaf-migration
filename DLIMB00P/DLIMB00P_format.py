36a7113a52bbab6917c119e9a17c8b1b6a4dffc1
pcolar <dave@pcolar.com>
2022-06-13 14:37:01 -0400

    DLIMB00P file updates Add files for DLIMB00P processing

---
+++ DLIMB00P/DLIMB00P_format.py
@@ -0,0 +1,11 @@
+DLIMB00P_field_format = [
+'BJI','A','20',
+'BJICG','A','3',
+'BJSTKSTS','A','3',
+'BJRLSD','A','10',
+'BJCSED','A','10',
+'BJFSIF','A','1',
+'BJLNKI','A','20',
+'BJPR1I','A','20',
+'BJPR2I','A','20',
+'BJPR3I','A','20']
---
+++ DLIMB00P/DLIMB00P_map.py
@@ -0,0 +1,8 @@
+field_map = {
+'BJI': 'I1I',
+'BJICG': 'I1GRP',
+'BJCSED': 'Final Return Date',
+'BJRLSD': 'First Due Date',
+'BJSTKSTS': 'I1STKR',
+'BJFSIF': 'Returnable'    
+}
